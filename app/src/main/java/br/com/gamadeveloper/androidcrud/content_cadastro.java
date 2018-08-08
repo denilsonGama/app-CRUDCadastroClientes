37fbce7de2e0f44210715189d688ab9fa6a1b3b7
Denilson Gama <denilson.gama@gamadeveloper.com.br>
2018-08-07 22:46:01 -0300

    second commit: Implementado DAO, Context, Tela para cadastro

--- app/src/main/java/br/com/gamadeveloper/androidcrud/content_cadastro.java
+++ app/src/main/java/br/com/gamadeveloper/androidcrud/content_cadastro.java
@@ -1,7 +1,14 @@
 package br.com.gamadeveloper.androidcrud;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class content_cadastro extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class content_cadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_cadastro);
+
+
+        Button btnSalvar = (Button)findViewById(R.id.btnSalvar);
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //carregando os campos
+                EditText txtNome = (EditText)findViewById(R.id.txtNome);
+                Spinner spnEstado = (Spinner)findViewById(R.id.spnEstado);
+                RadioGroup rgSexo = (RadioGroup)findViewById(R.id.rgSexo);
+                CheckBox chkVip = (CheckBox)findViewById(R.id.chkVip);
+
+                //pegando os valores
+                String nome = txtNome.getText().toString();
+                String uf = spnEstado.getSelectedItem().toString();
+                boolean vip = chkVip.isChecked();
+                String sexo = rgSexo.getCheckedRadioButtonId() == R.id.rbMasculino ? "M" : "F";
+
+                //salvando os dados
+                ClienteDAO dao = new ClienteDAO(getBaseContext());
+                boolean sucesso = dao.salvar(nome, sexo, uf, vip);
+                if(sucesso) {
+                    //limpa os campos
+                    txtNome.setText("");
+                    rgSexo.setSelected(false);
+                    spnEstado.setSelection(0);
+                    chkVip.setChecked(false);
+
+                    Snackbar.make(view, "Salvou!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    findViewById(R.id.includemain).setVisibility(View.VISIBLE);
+                    findViewById(R.id.includecadastro).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.fab).setVisibility(View.VISIBLE);
+                }else{
+                    Snackbar.make(view, "Erro ao salvar, consulte os logs!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            }
+        });
     }
 }
